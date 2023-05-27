23afd18c5a16ad233c859ba5b27e20ffb75a895e
pelumi <polalekan@gmail.com>
2023-05-27 02:31:39 +0100

    00 - task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Script for handling Personal Data
+"""
+from typing import List
+import re
+import logging
+from os import environ
+# import mysql.connector
+
+
+# # PII fields to be redacted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces sensitive information in a message with a redacted value
+    based on the list of fields to redact
+
+    Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+
+    Returns:
+        The filtered string message with redacted values
+    """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
---
+++ README.md
@@ -0,0 +1,1 @@
+# Alx Backend Specialisation- User Data
