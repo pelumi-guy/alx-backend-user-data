98e6f9cf7f30f7cbca8fe46a71a5f6fa825f527b
pelumi <polalekan@gmail.com>
2023-05-30 15:50:12 +0100

    01 - task 2 updated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -70,4 +70,4 @@ def forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
