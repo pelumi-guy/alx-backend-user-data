23e58d1a59897b32b679034efef1696f61adbf04
pelumi <polalekan@gmail.com>
2023-05-30 20:00:22 +0100

    01 - task 1 updated

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    GET /api/v1/unauthorized
+    """
+    abort(401, description="You are not authorized to access this resource")
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """
+    GET /api/v1/forbidden
+    """
+    abort(403, description="This resource is forbidden")
