34b89ab81b4c810ab865e3aeab81935cb2f485c3
pelumi <polalekan@gmail.com>
2023-06-07 01:43:29 +0100

    03 - updated db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,8 +41,9 @@ class DB:
         """
         new_user = User(email=email, hashed_password=hashed_password)
         # add new user and commit to database
-        self._session.add(new_user)
-        self._session.commit()
+        session = self._session
+        session.add(new_user)
+        session.commit()
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
